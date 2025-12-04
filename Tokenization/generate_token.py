c279068aaa883f70cefa49a302329f961412c143
wwz <102224941+wwzll123@users.noreply.github.com>
2025-12-04 12:28:05 +0800

    Update generate_token.py

--- Tokenization/generate_token.py
+++ Tokenization/generate_token.py
@@ -47,29 +47,20 @@ tokenizer_collection=get_esm3_model_tokenizers()
 function_tokenizer=InterProQuantizedTokenizer()
 str_tokenizer=tokenizer_collection.structure
 
-
-# interpro_path=r"F:\database\esm3_UniDRBP40_InterPro.json"
-# pdb_dir=r"F:\database\high_plddt_esm3_newDRBP"
-# ss_json_path=r"F:\database\esm3_DRBP_ss.json"
-# target_dir=r"F:\database\new_DRBP_token"
-plddt_json={}
-
-with open(config['ss_json_path'], 'r') as f:
+with open(config.ss_json_path, 'r') as f:
     ss_dicts=json.load(f)
 
-with open(config['interpro_path'], 'r') as f:
+with open(config.interpro_path, 'r') as f:
     inter_pro_dicts=json.load(f)
 
 
 def parse_string(input_str):
-    # 定义正则表达式模式
+
     pattern = r'(\w+)\((\d+)-(\d+)\)'
     match = re.match(pattern, input_str)
 
     if match:
-        # 提取括号前的内容
         prefix = match.group(1)
-        # 提取括号内的数字，并转换为整数列表
         numbers = [int(num) for num in match.groups()[1:]]
         return prefix, numbers
     else:return None,None
@@ -77,17 +68,15 @@ def parse_string(input_str):
 
 
 def generate_token(protein_id):
-    pdb_path=config['pdb_dir']+os.sep+protein_id+'.pdb'
-    target_path=config['target_dir'] + os.sep + f"{protein_id}_tokens.npz"
+    pdb_path=config.pdb_dir+os.sep+protein_id+'.pdb'
+    target_path=config.target_dir + os.sep + f"{protein_id}_tokens.npz"
     #if not os.path.exists(pdb_path) or os.path.exists(target_path):return
     chain = ProteinChain.from_pdb(pdb_path)
-    #plddt_json[protein_id] = round(chain.confidence.mean(), 4)
-    #if chain.confidence.mean()<70:return
-
+  
     protein = ESMProtein.from_protein_chain(chain)
-    #溶剂可及性
+
     protein.sasa=chain.sasa()
-    #二级结构
+
     protein.secondary_structure=ss_dicts[protein_id].replace('-','C')
     protein.function_annotations=[]
     if protein_id in inter_pro_dicts:
@@ -129,17 +118,11 @@ def generate_token(protein_id):
 
 
 if __name__ == '__main__':
-    # fasta_path=r"E:\science\EiRA\database\UniDRBP40_toUniBind.fasta"
-    # with open(fasta_path, 'r') as file:
-    #     protein_ids = [line[1:].strip() for line in file if line.startswith('>')]
-    protein_ids=np.loadtxt(config['protein_list_file'],dtype=str)
+    protein_ids=np.loadtxt(config.protein_list_file,dtype=str)
     for one_protein_id in tqdm(protein_ids):
-        target_path = config['target_dir'] + os.sep + f"{one_protein_id}_tokens.npz"
-        if not os.path.exists(config['pdb_dir']+os.sep+one_protein_id+'.pdb') or os.path.exists(target_path):continue
+        target_path = config.target_dir + os.sep + f"{one_protein_id}_tokens.npz"
+        if not os.path.exists(config.pdb_dir+os.sep+one_protein_id+'.pdb') or os.path.exists(target_path):continue
         try:
             generate_token(one_protein_id)
         except Exception as e:
             traceback.print_exc()
-
-# with open(r'F:\database\DRBP_ESM3_plddt.json', 'w') as f:
-#     json.dump(plddt_json, f)
